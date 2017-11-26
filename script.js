33cf9018a166269d6f50e875569565f0af260590
IhabTag <30555200+IhabTag@users.noreply.github.com>
2017-11-26 16:12:25 +0200

    3

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ function ageCalc(myAge) {
     
     "use strict";
     
+    var calcAge;
+    
     myAge = document.getElementById("ageInYears").value;
     
-    return (document.getElementById("resultText").innerHTML = "Age in Days = " +
-            "<span style='font-weight: bold;'>" + (myAge * 365) + "</span> days");
+    if (myAge > 0 && myAge < 200) {
+        
+        calcAge = myAge * 365;
+        
+        return (document.getElementById("resultText").innerHTML = "Age in Days = " +
+            "<span style='font-weight: bold;'>" + calcAge + "</span> days");
+    } else {
+        calcAge = (document.getElementById("resultText").innerHTML = "Please enter a valid age!");
+    
+        return (document.getElementById("resultText").innerHTML = "<span style='font-weight: bold;'>" + calcAge + "</span>");
+    }
 }
